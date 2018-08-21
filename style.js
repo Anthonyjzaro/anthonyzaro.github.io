b63c1f69b75b0bc3675c58e148f355d781ed9328
Anthony Zaro <anthonyjzaro@gmail.com>
2018-08-20 20:22:47 -0400

    update

---
+++ style.js
@@ -0,0 +1,7 @@
+$("button").click(function(){
+    var div = $("div");
+    div.animate({height: '300px', opacity: '0.4'}, "slow");
+    div.animate({width: '300px', opacity: '0.8'}, "slow");
+    div.animate({height: '100px', opacity: '0.4'}, "slow");
+    div.animate({width: '100px', opacity: '0.8'}, "slow");
+}); 
